abd767cb100eb592e0006ed4cdae568f117e32fe
robertjankowski <robjanw11@wp.pl>
2019-03-13 22:01:06 +0100

    sample ui fxml solution

---
+++ src/ui/MenuController.java
@@ -0,0 +1,32 @@
+package ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class MenuController {
+
+    @FXML
+    public void showHelp() {
+        final Stage dialog = new Stage();
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        VBox dialogVBox = new VBox(20);
+        dialogVBox.getChildren().add(new Text(aboutUs()));
+        Scene dialogScene = new Scene(dialogVBox, 300, 200);
+        dialog.setScene(dialogScene);
+        dialog.show();
+
+    }
+
+
+    private String aboutUs() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n\tCellular automata simulation program\n");
+        sb.append("\tCreated by Łukasz Bożek & Robert Jankowski\n");
+        return sb.toString();
+    }
+
+}
